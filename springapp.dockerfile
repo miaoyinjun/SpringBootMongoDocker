4dd5bb8a5c10303638dd8d470455d6ebf517e336
jimmy <miaoyinjun@gmail.com>
2017-06-12 15:32:49 +0800

    init

--- springapp.dockerfile
+++ springapp.dockerfile
@@ -1,16 +1,11 @@
-FROM maven:3.3.3
+FROM registry.cn-hangzhou.aliyuncs.com/jimmy_miao/spring_boot_mongodb
 
-ADD pom.xml /tmp/build/
-RUN cd /tmp/build && mvn -q dependency:resolve
+#ENV appName SpringBootMongoDocker-1.0-SNAPSHOT.jar
 
-ADD src /tmp/build/src
-        #构建应用
-RUN cd /tmp/build && mvn -q -DskipTests=true package \
-        #拷贝编译结果到指定目录
-        && mv target/*.jar /app.jar \
-        #清理编译痕迹
-        && cd / && rm -rf /tmp/build
-		
-VOLUME /tmp
-EXPOSE 8080
-ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
+#ADD target/SpringBootMongoDocker-1.0-SNAPSHOT.jar /opt/spring-boot-maven-docker/
+
+EXPOSE 8080 5005
+
+#WORKDIR /opt/spring-boot-maven-docker/
+
+CMD ["java", "-jar", "SpringBootMongoDocker-1.0-SNAPSHOT.jar"]
--- src/main/java/cn/com/controllers/IndexController.java
+++ src/main/java/cn/com/controllers/IndexController.java
@@ -3,9 +3,14 @@ package cn.com.controllers;
 import cn.com.domain.User;
 import org.mongodb.morphia.Datastore;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by xiaxuan on 16/12/12.
@@ -26,4 +31,20 @@ public class IndexController {
         datastore.save(user);
         return user;
     }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET,
+            produces = "application/json;charset=utf-8")
+    public List<User> home() {
+        System.out.println("11121212121212121212");
+//        return datastore.createQuery(User.class).asList();
+        List<User> list = new ArrayList<>();
+        list.add(new User());
+        return list;
+    }
+
+    @ExceptionHandler
+    public String getErrorResult(HttpServletRequest request, Exception ex) {
+       return ex.getMessage()+ex.getClass();
+    }
+
 }
